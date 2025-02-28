dfdb19da53d0a0f11d8ad8cf54b82dcda0c6f5fe
Osa <amenokamiosaka@gmail.com>
2025-02-28 11:37:27 +0700

    Delete reviewarray directory

--- reviewarray/src/Evelin/Larik.java
+++
@@ -1,9 +0,0 @@
-package Evelin;
-public class Larik {
-    public static void cetak(int[] x){
-        for (int i = 0; i < x.length; i++) {
-            if (i == x.length - 1)System.out.print(x[i]);
-            else System.out.print(x[i]+", ");
-        }
-    }
-}
